3e53ec6f47b551823a3b7733199d37aabf33f688
zhongjun2 <jun.zhongjun@huawei.com>
2018-08-01 11:09:16 +0800

    Update config_validator.rb

--- lib/validator/config_validator.rb
+++ lib/validator/config_validator.rb
@@ -38,7 +38,6 @@ module Validator
           },
           'validator' => {
               'network_id' => ReplacedString.new,
-              'vpc_id' => ReplacedString.new,
               'floating_ip' => ReplacedString.new,
               'static_ip' => ReplacedString.new,
               'private_key_path' => String,
